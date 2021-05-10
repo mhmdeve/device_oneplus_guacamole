7153cd7655fcf1f842c3f13b7e845c24ac97f9ab
Nolen Johnson <johnsonnolen@gmail.com>
2021-05-10 13:36:52 +0000

    guacamole: Simplify AVB flag logic
    * AVB's `--flags 3` arguement builds a disabled VBMeta with
      disabled hashtree, so no need to set both flags individually.
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -169,8 +169,7 @@ PRODUCT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn
