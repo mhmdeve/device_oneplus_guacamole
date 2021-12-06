b3f33ebe5495b31a13a649d8158ce4624a38e416
HotDogfinba11 <https://olivers-portfolio.co.uk>
2021-12-06 15:51:39 +0530

    sm8250-common: Adaptive charging xml
    Allows to enable adaptive charging toggle.
    
    Signed-off-by: sakhiofsakshi <imsakshisharma04@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: c79 <vexed@riseup.net>
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Change-Id: Ifb26b4033d58e333c278df52c01fa7d8ac246322

--- device.mk
+++ device.mk
@@ -78,6 +78,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# Adaptive charging
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/adaptivecharging.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/adaptivecharging.xml
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
