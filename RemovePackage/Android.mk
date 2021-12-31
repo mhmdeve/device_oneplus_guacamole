fa740b779e2d0e92a088fbc2466c8b3886b9e5b4
Master <mhmdeveloperofficial@gmail.com>
2021-12-31 06:31:17 +0000

    guacamole: Removed Some Packages

---
+++ RemovePackage/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := Maps PrebuiltGmail YouTubeMusicPrebuilt TipsPrebuilt GoogleTTS Music MicropaperPrebuilt WallpapersBReel2020 PlayGames Drive Photos KeepNotes Keep YouTube PixelBuds
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
