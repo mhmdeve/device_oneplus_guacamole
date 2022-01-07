f44a6de9f915004565b40c2a6b026b15d3134121
alk3pInjection <webmaster@raspii.tech>
2022-01-08 00:45:12 +0800

    Reapply "sm8150-common: implement ALS compensation algorithm"
    This reverts commit 807ded7e4a8d6a0154c5dfc5f42311560db48a21.
    
    Change-Id: I94599161ec471ab89fc1a061b8b5343cdec032a9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,6 +57,9 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 
+# Treble
+PRODUCT_FULL_TREBLE_OVERRIDE := true
+
 # A/B
 AB_OTA_UPDATER := true
 
