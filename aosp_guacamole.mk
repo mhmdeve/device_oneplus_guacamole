e5e7362059fb05965387e873721a3f3266f6b46c
Master <mhmdeveloperofficial@gmail.com>
2022-03-02 10:15:00 +0000

    guacamole: Bring Up PixelPlusUI

--- aosp_guacamole.mk
+++ aosp_guacamole.mk
@@ -11,11 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from guacamole device
 $(call inherit-product, device/oneplus/guacamole/device.mk)
 
-# Inherit some common krypton stuff.
-$(call inherit-product, vendor/krypton/config/common_full_phone.mk)
+# Inherit some common PixelPlusUI stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Moto Dolby
+$(call inherit-product, vendor/oneplus/dolby/dolby-vendor.mk)
+
+# GoogleCamera
+$(call inherit-product, vendor/GoogleCamera/config.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := krypton_guacamole
+PRODUCT_NAME := aosp_guacamole
 PRODUCT_DEVICE := guacamole
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := OnePlus 7 Pro
@@ -28,12 +34,20 @@ PRODUCT_AAPT_CONFIG := xxxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 PRODUCT_CHARACTERISTICS := nosdcard
 
-# OOS Gallery
-TARGET_SHIPS_OOSGALLERY := false
+#PixelPlus-UI Official Stuff
+CUSTOM_BUILD_TYPE := OFFICIAL
+IS_PHONE := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+PPUI_MAINTAINER := Master
+TARGET_SUPPORTS_QUICK_TAP  := true
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 3120
 TARGET_SCREEN_WIDTH := 1440
+TARGET_BOOT_ANIMATION_RES := 1440
 
 # Build info
 BUILD_FINGERPRINT := "OnePlus/OnePlus7Pro/OnePlus7Pro:11/RKQ1.201022.002/2202112154:user/release-keys"
--- device.mk
+++ device.mk
@@ -12,7 +12,7 @@ $(call inherit-product, vendor/oneplus/guacamole/guacamole-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-krypton
+    $(LOCAL_PATH)/overlay-aosp
 
 # Audio
 PRODUCT_COPY_FILES += \
