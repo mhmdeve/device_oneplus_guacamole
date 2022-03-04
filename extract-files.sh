7699f3146b3949daebfba7374c779121a6742f9b
alk3pInjection <webmaster@raspii.tech>
2022-03-04 16:12:18 +0800

    Revert "guacamole: Drop QCOM WFD"
    This reverts commit 79197a900bdbd69069b063806729a6150cbe61bc.
    
    Change-Id: I9eb9dc1e80e206f8ca4923f7f75e6ad1109f34fa

--- extract-files.sh
+++ extract-files.sh
@@ -55,6 +55,9 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        system_ext/lib64/libwfdnative.so)
+            sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
+            ;;
         vendor/etc/qdcm_calib_data_*.xml)
             python - "${2}" <<EOF
 import sys
