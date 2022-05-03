92b50f422b6caafefe1091c7fd8b6d15c584367f
MHMDeveloper <mhmdeveloperofficial@gmail.com>
2022-05-03 18:06:17 +0500

    guacamole: Bring Up EvoX

--- evolution_guacamole.mk
+++ evolution_guacamole.mk
@@ -11,28 +11,28 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from guacamole device
 $(call inherit-product, device/oneplus/guacamole/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common EvolutionX stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Moto Dolby
 $(call inherit-product, vendor/oneplus/motorola/dolby-vendor.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_guacamole
+PRODUCT_NAME := evolution_guacamole
 PRODUCT_DEVICE := guacamole
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := GM1917
+PRODUCT_MODEL := GM1913
 PRODUCT_MANUFACTURER := OnePlus
 
 PRODUCT_AAPT_CONFIG := xxxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 PRODUCT_CHARACTERISTICS := nosdcard
 
-# Device maintainer
-DEVICE_MAINTAINER := alk3p
-
-# Custom Stuff
+# Evolution Stuff
+DEVICE_MAINTAINER := MHMDeveloper
+EVO_BUILD_TYPE := SPECIAL
 TARGET_SUPPORTS_QUICK_TAP  := true
+TARGET_SHIP_PREBUILT_APEX := true
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1440
