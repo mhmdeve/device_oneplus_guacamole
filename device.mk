3d3c58cdffac99cee22e761e81c05fecc5fb96ad
alk3pInjection <webmaster@raspii.tech>
2022-05-07 18:53:50 +0800

    Revert "fixup! guacamole: Build NFC from source"
    nfc configs are imported from
      yaap/device_oneplus_sm8150-common@74cdbe4
      yaap/device_oneplus_sm8150-common@a9b8aff
    
    This reverts commit ada6666030b821daf89609264d9b78068f863fba.
    
    Change-Id: Ic03ee1fb067b6a89edbf1976c4f733bd3fad472f

--- device.mk
+++ device.mk
@@ -77,7 +77,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
-    frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
+    frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
 
 # ANT+
 PRODUCT_PACKAGES += \
@@ -348,9 +349,14 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2.vendor \
-    com.android.nfc_extras \
+    android.hardware.nfc@1.2-service \
     NfcNci \
+    SecureElement \
     Tag
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # OMX
 PRODUCT_PACKAGES += \
