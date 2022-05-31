b143668fba4f2d77fac088981a9e8fdd2b08343a
Master <mhmdeveloperofficial@gmail.com>
2022-05-31 18:48:23 +0600

    guacamole: reimport OnePlus Apps

--- aosp_guacamole.mk
+++ aosp_guacamole.mk
@@ -18,8 +18,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 $(call inherit-product, vendor/motorola/dolby/dolby-vendor.mk)
 
 # OnePlus apps
-$(call inherit-product-if-exists, vendor/oneplus/apps/guacamole/config.mk)
-$(call inherit-product-if-exists, vendor/addons/oneplus/apps/config.mk)
+$(call inherit-product-if-exists, vendor/oneplus/apps/apps-vendor.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_guacamole
