6576d69c5adcb60eb73a3623e90a10c43646dfb7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-29 09:29:32 +0200

    guacamole: Switch to two-stage init mounting
    * Use correct mount flags coming from kernel dts
    * Move system mount point to /dev/block/by-name/, we don't have
      bootdevice symlink to that point in init
    * Copy fstab to recovery for first stage init
    
    Change-Id: Iaa0ecc3f13c3cfd3fe357ca509549d0f68a29cf0

--- device.mk
+++ device.mk
@@ -19,6 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/first_stage_ramdisk/fstab.qcom
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
