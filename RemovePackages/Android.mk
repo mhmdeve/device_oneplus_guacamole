0380d6e3427ecf01a417fcd0528ca0dfe20ea753
Master <mhmdeveloperofficial@gmail.com>
2022-08-28 10:15:44 +0000

    guacamole: Nuke few more packages

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps YouTubeMusicPrebuilt RecorderPrebuilt PlayGames PixelBuds
+LOCAL_OVERRIDES_PACKAGES := Maps YouTubeMusicPrebuilt RecorderPrebuilt PlayGames PixelBuds AudioFx Camera2 SoundAmplifierPrebuilt AudioFX MusicFX
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
