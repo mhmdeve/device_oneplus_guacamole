0c35c778bef56769165b55e5711d63011d2f683c
Master <86474801+mhmdeve@users.noreply.github.com>
2022-10-22 10:10:36 +0500

    guacamole: Update vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,19 +1,18 @@
 # Clone/Fetch Upstream Device Dependencies
 # Device Tree
 echo "Cloning OnePlus 7Pro Tree"
-git clone https://github.com/ProjectElixir-Devices/android_device_oneplus_guacamole -b Tiramisu device/oneplus/guacamole
+git clone https://github.com/PixelPlusUI-Devices/device_oneplus_guacamole -b tiramisu device/oneplus/guacamole
 echo ""
 
 echo "Cloning Vendor blobs"
-git clone https://github.com/ProjectElixir-Devices/vendor_oneplus_guacamole -b Tiramisu vendor/oneplus/guacamole
+git clone https://github.com/PixelPlusUI-Devices/vendor_oneplus_guacamole -b tiramisu vendor/oneplus/guacamole
 echo ""
 
 echo "Cloning kernel tree"
-git clone https://github.com/ProjectElixir-Devices/kernel_oneplus_guacamole kernel/oneplus/sm8150
+git clone https://github.com/PixelPlusUI-Devices/kernel_oneplus_guacamole kernel/oneplus/sm8150
 echo ""
 
 # OnePlus Stuff 
 echo "Cloning OnePlus"
 git clone https://gitlab.com/PixelPlusUI-SnowCone/vendor_oneplus_apps vendor/oneplus/apps
-git clone https://github.com/PixelPlusUI-Devices/vendor_motorola_dolby vendor/motorola/dolby
 echo ""
