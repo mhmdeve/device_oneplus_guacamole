3065ca4c09d5626418f363f8e9022b0bf5dd2e28
Pranav Vashi <neobuddy89@gmail.com>
2022-12-03 21:12:56 +0530

    guacamole: Add Moto dolby support
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -251,6 +251,13 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm-service.clearkey
 
+# Dolby
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml \
+    $(LOCAL_PATH)/configs/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
+
+TARGET_EXCLUDES_AUDIOFX := true
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
