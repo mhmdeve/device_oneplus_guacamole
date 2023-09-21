dfba06c04a50963e98da744136da517f3a7e3b92
Master <mhmdeveloperofficial@gmail.com>
2023-09-21 04:05:34 +0000

    guacamole: Bring up EVO X

--- device.mk
+++ device.mk
@@ -22,7 +22,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay-evolution
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
--- evolution_guacamole.mk
+++ evolution_guacamole.mk
@@ -12,14 +12,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from guacamole device
 $(call inherit-product, device/oneplus/guacamole/device.mk)
 
-# Inherit some common Arrow stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Evolution stuff.
+$(call inherit-product, vendor/evolution/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_guacamole
+PRODUCT_NAME := evolution_guacamole
 PRODUCT_DEVICE := guacamole
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := GM1917
+PRODUCT_MODEL := GM1913
 PRODUCT_MANUFACTURER := OnePlus
 
 PRODUCT_SYSTEM_NAME := OnePlus7Pro
@@ -29,17 +29,26 @@ PRODUCT_AAPT_CONFIG := xxxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 PRODUCT_CHARACTERISTICS := nosdcard
 
-# Device maintainer
-DEVICE_MAINTAINER := alk3p
-
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1440
 
-# Build info
-BUILD_FINGERPRINT := "OnePlus/OnePlus7Pro/OnePlus7Pro:11/RKQ1.201022.002/2204061044:user/release-keys"
+# Custom stuff.
+TARGET_BUILD_APERTURE_CAMERA := true
+TARGET_USE_PIXEL_FINGERPRINT := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
+EXTRA_UDFPS_ANIMATIONS := true
+EVO_BUILD_TYPE := SPECIAL
+
+# Gapps Build Details
+TARGET_GAPPS_ARCH := arm64
+
+# Build Fingerprint
+BUILD_FINGERPRINT := "OnePlus/OnePlus7Pro/OnePlus7Pro:11/RKQ1.201022.002/2206171030:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=OnePlus7Pro \
     TARGET_PRODUCT=OnePlus7Pro \
-    PRIVATE_BUILD_DESC="qssi-user 11 RKQ1.201022.002 2204061044 release-keys"
+    PRIVATE_BUILD_DESC="OnePlus7Pro-user 11 RKQ1.201022.002 2206171030 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
