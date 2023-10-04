b7129338da8a4c6f0d1aa15ab9a9ff509097cf9f
Sarthak Roy <sarthakroy2002@gmail.com>
2023-10-04 20:36:50 +0530

    guacamole: Switch to Wifi service AIDL
    Change-Id: I5a2864b4159b7815eff4dd2f0de02a3dabbacfa5
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- device.mk
+++ device.mk
@@ -508,7 +508,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     libwpa_client \
     libwifi-hal-ctrl \
