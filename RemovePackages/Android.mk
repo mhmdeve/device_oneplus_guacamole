f326b1f46417116f66aca39c47ee5f1f2f8bb553
Master <mhmdeveloperofficial@gmail.com>
2023-10-17 07:13:45 +0000

    guacamole: Remove more packages

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps YouTubeMusicPrebuilt RecorderPrebuilt PlayGames PixelBuds AudioFx Camera2 SoundAmplifierPrebuilt AudioFX MusicFX PixelLiveWallpaperPrebuilt Papers EvoEgg NgaResources MaestroPrebuilt DiagnosticsToolPrebuilt Photos Gmail ScribePrebuilt Showcase
+LOCAL_OVERRIDES_PACKAGES := PrebuiltGmail YouTube Drive MyVerizonServices Maps YouTubeMusicPrebuilt RecorderPrebuilt PlayGames PixelBuds AudioFx Camera2 SoundAmplifierPrebuilt AudioFX MusicFX PixelLiveWallpaperPrebuilt Papers EvoEgg NgaResources MaestroPrebuilt DiagnosticsToolPrebuilt Photos Gmail ScribePrebuilt Showcase
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
