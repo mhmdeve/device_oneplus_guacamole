c5caf3070932f2d0cbc42524512f2ef9762759b2
SGCMarkus <markusornik@gmail.com>
2023-10-19 04:49:05 +0000

    guacamole: Set Board API level

--- aosp_guacamole.mk
+++ aosp_guacamole.mk
@@ -7,7 +7,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit from guacamole device
 $(call inherit-product, device/oneplus/guacamole/device.mk)
