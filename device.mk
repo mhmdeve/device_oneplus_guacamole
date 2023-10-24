e7e3bc4d9a675331e911e3f032d696b1ec73c971
k001droid <20264908+k001droid@users.noreply.github.com>
2023-10-24 11:28:50 +0000

    guacamole: Compile libldacBT_bco.so 08-17 12:10:55.180 19022 19075 E a2dp_vendor_ldac_decoder: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library "libldacBT_bco.so" not found
    Change-Id: I910382a3affe8d02d6b0b20fd2dc3e91e57f3e9a

--- device.mk
+++ device.mk
@@ -121,6 +121,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     liba2dpoffload \
     libbthost_if \
+    libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
