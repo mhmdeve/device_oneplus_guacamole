911364f9b34b8d9b04092e46299bc9eea9b19e6b
Master <mhmdeveloperofficial@gmail.com>
2023-11-03 04:49:21 +0000

    guacamole: ViPER4Android FX support

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,4 @@ git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x
 git clone https://github.com/mhmdeve/vendor_oneplus_guacamole -b evolution vendor/oneplus/guacamole
 git clone https://github.com/mhmdeve/kernel_oneplus_sm8150 -b evolution kernel/oneplus/sm8150
 git clone https://gitlab.com/PixelPlusUI-SnowCone/vendor_oneplus_apps vendor/oneplus/apps
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
