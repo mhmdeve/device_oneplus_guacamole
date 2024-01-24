123f3ca86ce5c58c6b481f5fb846124b357507fa
Master <mhmdeveloperofficial@gmail.com>
2024-01-24 15:30:06 +0000

    guacamole: vibrator: Run clang-format

--- vibrator/service.cpp
+++ vibrator/service.cpp
@@ -37,14 +37,17 @@
 
 using aidl::android::hardware::vibrator::Vibrator;
 
-int main() {
-    ABinderProcess_setThreadPoolMaxThreadCount(0);
-    std::shared_ptr<Vibrator> vib = ndk::SharedRefBase::make<Vibrator>();
+int main()
+{
+	ABinderProcess_setThreadPoolMaxThreadCount(0);
+	std::shared_ptr<Vibrator> vib = ndk::SharedRefBase::make<Vibrator>();
 
-    const std::string instance = std::string() + Vibrator::descriptor + "/default";
-    binder_status_t status = AServiceManager_addService(vib->asBinder().get(), instance.c_str());
-    CHECK(status == STATUS_OK);
+	const std::string instance =
+		std::string() + Vibrator::descriptor + "/default";
+	binder_status_t status = AServiceManager_addService(
+		vib->asBinder().get(), instance.c_str());
+	CHECK(status == STATUS_OK);
 
-    ABinderProcess_joinThreadPool();
-    return EXIT_FAILURE;  // should not reach
+	ABinderProcess_joinThreadPool();
+	return EXIT_FAILURE; // should not reach
 }
