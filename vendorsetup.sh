b20568de2a5af72b643e7a86df8580c20cf9e832
Master <mhmdeveloperofficial@gmail.com>
2024-02-18 04:48:41 +0000

    guacamole: Update clang to 18.0.0

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r498229b prebuilts/clang/host/linux-x86/clang-r498229b --depth=1
+git clone https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r510928 prebuilts/clang/host/linux-x86/clang-r510928 --depth=1
 git clone https://github.com/mhmdeve/vendor_oneplus_guacamole -b evolution vendor/oneplus/guacamole
 git clone https://github.com/mhmdeve/kernel_oneplus_sm8150 -b evolution kernel/oneplus/sm8150
 git clone https://gitlab.com/PixelPlusUI-SnowCone/vendor_oneplus_apps vendor/oneplus/apps
